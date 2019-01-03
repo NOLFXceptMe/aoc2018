1d20cc9dd21a53b8c945d20339b72239d999f0e8
Naveen Kumar Molleti <nerd.naveen@gmail.com>
2019-01-03 00:08:11 -0800

    Day 2 WIP

---
+++ day2/part1.rkt
@@ -0,0 +1,44 @@
+#lang racket
+
+(define input (file->lines "day2.input"))
+
+(define encode-char
+  (lambda (char)
+    (let ([index (- (char->integer char) (char->integer #\a))])
+      (arithmetic-shift 1 (* 2 index)))))
+
+(define encode-word
+  (lambda (word)
+    (let ([chars (string->list word)])
+      (foldl + 0 (map encode-char chars)))))
+
+(define encoded-words (map encode-word input))
+
+(define indices (range 0 26))
+
+(define extract
+  (lambda (number index)
+    (bitwise-and
+      number
+      (+ (arithmetic-shift 1 (* 2 index))
+         (arithmetic-shift 1 (+ 1 (* 2 index)))))))
+
+(define reduce
+  (lambda (number index)
+    (arithmetic-shift number (* -2 index))))
+
+(define has-at-i?
+  (lambda (encoded-word number)
+    (lambda (i)
+      (bitwise-xor (reduce (extract encoded-word i) i) number))))
+
+(define has? (lambda (number)
+  (lambda (encoded-word)
+    (if (member 0 (map (has-at-i? encoded-word number) indices))
+      1 0)))
+  )
+
+(define words (lambda (number)
+  (foldl + 0 (map (has? number) encoded-words))))
+
+(println (list (words 2) (words 3)))
